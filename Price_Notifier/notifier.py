b8e895507e7089f5f1f53643c7cb4693d6573e2e
An Nguyen <tuh09895@temple.edu>
2020-01-06 05:35:40 +0000

    Parsed in the title of the product :D

--- Price_Notifier/notifier.py
+++ Price_Notifier/notifier.py
@@ -1,15 +1,21 @@
 import bs4
 import requests
+from bs4 import BeautifulSoup
+import urllib2
+
 
 #URL of the product wanted 
-URL = "https://www.amazon.com/Introduction-Algorithms-3rd-MIT-Press/dp/0262033844?pf_rd_p=dd4f5b57-ba5b-418b-a348-64ec62df14cc&pd_rd_wg=06bmi&pf_rd_r=KK0D5A466X1SM48PE4YM&ref_=pd_gw_cr_simh&pd_rd_w=9pCZy&pd_rd_r=bdd8fa57-eaf6-4824-9902-02a30b50617c"
-
+URL = 'https://www.amazon.com/gp/product/B07RF2123Z/ref=s9_acss_bw_cg_PCLTMC_2b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=C2EYPD896DK1G7KM0PS3&pf_rd_t=101&pf_rd_p=e5059426-911c-49a1-830e-fa1d8c1fc535&pf_rd_i=565108'
 #Headers used for requests function call to identify OS and web browser 
 
-headers = {'User-Agent' :   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36' }
+headers = {"User-Agent" :   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36' }
 
-page_request = requests.get(URL, headers = headers)
+page_request = requests.get(URL, headers=headers)
 
-print(page_request)
- 
+contents = page_request.content
 
+soup = BeautifulSoup(contents, 'html.parser')
+soup1 = BeautifulSoup(soup.prettify(), 'html.parser')
+#print(soup.prettify())
+title = soup1.find(id='productTitle')
+print(title)
