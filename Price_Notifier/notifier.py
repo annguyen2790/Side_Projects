6388788f4ef017d847e4abbf829eddc0adc85fe8
An Nguyen <tuh09895@temple.edu>
2020-01-09 04:08:46 +0000

    Added password input :D

--- Price_Notifier/notifier.py
+++ Price_Notifier/notifier.py
@@ -2,7 +2,7 @@ import bs4
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-
+import time
 def checkPrice():
     #URL of the product wanted 
     URL = 'https://www.amazon.com/gp/product/B07RF2123Z/ref=s9_acss_bw_cg_PCLTMC_2b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=C2EYPD896DK1G7KM0PS3&pf_rd_t=101&pf_rd_p=e5059426-911c-49a1-830e-fa1d8c1fc535&pf_rd_i=565108'
@@ -14,8 +14,8 @@ def checkPrice():
 
     contents = page_request.content
 
-    soup = BeautifulSoup(contents, 'html.parser')
-    soup1 = BeautifulSoup(soup.prettify(), 'html.parser')
+    soup = BeautifulSoup(contents, 'html.parser') #make a soup object 
+    soup1 = BeautifulSoup(soup.prettify(), 'html.parser') #make a second soup object with all the HTML parsed correctly from the first soup object
     #print(soup.prettify())
     title = soup1.find(id='productTitle').get_text().strip()
     #print(title)
@@ -23,16 +23,15 @@ def checkPrice():
     price = float(price_in_text[1:7])
     print(price)
     #send_email
+    return price
 
-def sent_email():
+def sent_email(password):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('timbuiild@gmail.com', 'dhfjd22')
-    subject = '\Reduced price!'
-    body = 'Click on this link! https://www.amazon.com/gp/product/B07RF2123Z/ref=s9_acss_bw_cg_PCLTMC_2b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=C2EYPD896DK1G7KM0PS3&pf_rd_t=101&pf_rd_p=e5059426-911c-49a1-830e-fa1d8c1fc535&pf_rd_i=565108'
-    msg = 'hello'
+    server.login('mazhurt@gmail.com', password)
+    msg = 'Price Fell!'
     server.sendmail(
         'mazhurt@gmail.com',
         'annguyen2790111@gmail.com',
@@ -47,5 +46,12 @@ def sent_email():
 
     
 if __name__ == "__main__":
-    checkPrice()
-    sent_email()
+    #email = input("Enter your email: ")
+    password = raw_input("Enter your password: ")
+    price = checkPrice()
+    while(True):
+        if price > 450:
+            sent_email(password)
+            time.sleep(86400)
+        else:
+            break
